daa405ecd3bca6c36cceadb6f0faea8c7f584a33
Pasindu Bhagya <63937160+PasinduBhagya@users.noreply.github.com>
2024-08-16 06:31:25 +0530

    Update MysqlTestApplication.java

--- src/test/java/org/springframework/samples/petclinic/MysqlTestApplication.java
+++ src/test/java/org/springframework/samples/petclinic/MysqlTestApplication.java
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+/*This is my comment to trigeer Jenkins*/
+
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
